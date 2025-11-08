81a43e93653cf786fee4d87ad630b2e4eab8b534
ShamiNet <shami313p@gmail.com>
2025-11-08 22:22:18 +0300

    Revert "feat: Implement comprehensive PDF report generation"
    This reverts commit 7e49093a7fbe93bde67ee8716a0d4f4cd37f2c9f.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,6 @@ import Foundation
 
 import cloud_firestore
 import cloud_functions
-import device_info_plus
-import file_saver
 import file_selector_macos
 import firebase_auth
 import firebase_core
@@ -23,8 +21,6 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
